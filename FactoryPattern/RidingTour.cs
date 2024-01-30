fbb55907a042e825df12b6656693dcfa676829f5
will street <will@williamdstreet.com>
2024-01-30 14:16:47 -0600

    first commit

--- FactoryPattern/RidingTour.cs
+++ FactoryPattern/RidingTour.cs
@@ -1,32 +1,20 @@
-﻿namespace FactoryPattern
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
 {
-    public class Program
+    internal class RidingTour : ITour
     {
-        static void Main(string[] args)
+        public void Tour()
         {
-            Console.WriteLine("Hello, Welcome to the automatic check-in at Jurassic Park Bank");
-            Console.WriteLine("John Hammond and everyone working at Jurassic Park, hopes that your visit is everything you expect and more");
-            Console.WriteLine("May I please have your name?");
-            string userName = Console.ReadLine();
-            Console.WriteLine($"Ah welcome, {userName}! We are delighted to have you staying with us.");
-            Console.WriteLine("I have your reservations right here, and one final question.");
-            Console.WriteLine("Will you be wanting to take the walking tour, or the tour that you will be riding? ");
-            Console.WriteLine("Please state 'walking' or 'riding'");
-            string userTourPick = Console.ReadLine();
-
-            ITour tour = TourFactory.GetTour(userTourPick);
-            tour.Tour();
-            
-
-
+            Console.WriteLine("The riding tour, wonderful. You will get a expirence that is quicker, but you will get to take in more of what we offer and in comfort.");
         }
+
     }
 }
-
-
-
-
-
 //Create a console app that utilizes factory pattern by taking a user’s input of how many tires are on a vehicle and, based on their input, creates that type of vehicle.
 //Complete this using an interface.
 //You must have at least 2 subclasses.
